a6334c8635c253ebdc44f91f7bd0ff9c66cadc67
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-03-05 23:18:48 +0200

    Adds basic routing setup

---
+++ src/app/pages/Start.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Start() {
+  return <Link to="/game">Start the Game</Link>;
+}
+
+export default Start;
---
+++ src/app/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Game } from './Game';
+export { default as Start } from './Start';
+export { default as Finish } from './Finish';

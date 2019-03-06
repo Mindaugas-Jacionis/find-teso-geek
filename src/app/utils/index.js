52a3889a6a8947f1931dcef6df0dbd06b8bd8337
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-03-06 23:44:28 +0200

    Adds basic firebase setup

---
+++ src/app/utils/index.js
@@ -0,0 +1,1 @@
+export { default as firebaseSetup } from './firebaseSetup';
--- src/demo/actions.js
+++ src/demo/actions.js
@@ -1,6 +1,20 @@
+import { firebaseSetup } from '~/app/utils';
 import * as types from './actionTypes';
 
+// const locale = 'lt-LT';
+// const DB_TABLE = new Date().toLocaleDateString(locale).replace(/-/g, '');
+// const playersRef = Fire.database().ref(DB_TABLE);
+
+const DB_DOC = firebaseSetup.database().ref('demo');
+
+// const DB_REF = firebase.database().ref();
+// const DB_DOC = DB_REF.child("todos");
+
 export const demo = () => dispatch => {
+  const { key } = DB_DOC.update({ count: Math.floor(Math.random() * 10000) });
+
+  console.log('key', key);
+
   setTimeout(() => {
     dispatch({ type: types.DEMO });
   }, 1000);

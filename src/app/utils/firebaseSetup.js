56ad49f1152be3a5ce0c88383ba8845ba7ad14dc
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-03-07 23:38:07 +0200

    Adds dotenv for firebase credentials

--- src/app/utils/firebaseSetup.js
+++ src/app/utils/firebaseSetup.js
@@ -2,6 +2,3 @@ import * as firebase from 'firebase';
 import config from '../config';
 
 export default firebase.initializeApp(config);
-
-// const databaseRef = firebase.database().ref();
-// export const todosRef = databaseRef.child("todos");

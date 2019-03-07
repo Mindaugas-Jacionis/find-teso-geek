b618f86dfe9a44a5641694bb9797b61a93d63e95
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-03-08 00:40:42 +0200

    Initial register form setup

--- src/app/pages/Start.js
+++ src/app/pages/Start.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { Form, Field } from 'react-final-form';
 
-import demo from '~/demo';
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
 
-function Start({ demoValue, demoAction }) {
+const validate = ({ name, email, field, year, degree }) => {
+  if (!name || !email || !field || !year || !degree) {
+    return { hasErrors: true };
+  }
+
+  return {};
+};
+
+function Start({ history }) {
+  console.log('Hi', history);
+  const onSubmit = data => {
+    console.log('I will create new user', data);
+    history.replace('/game');
+  };
+
   return (
-    <div>
-      <Link to="/game">Start the Game</Link>
-      <button type="button" onClick={demoAction}>
-        {demoValue}
-      </button>
-    </div>
+    <Container>
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        render={({ handleSubmit, pristine, invalid }) => (
+          <form onSubmit={handleSubmit}>
+            <h1>Take the challenge!</h1>
+            <div>
+              <Field name="name" component="input" placeholder="Full Name" />
+              <Field type="email" name="email" component="input" placeholder="Email" />
+            </div>
+            <div>
+              <Field name="field" component="input" placeholder="Field of Study" />
+              <div>
+                <Field
+                  min="0"
+                  type="number"
+                  name="year"
+                  component="input"
+                  placeholder="Year of Study"
+                />
+                <Field name="degree" component="input" placeholder="Degree" />
+              </div>
+            </div>
+            <p>By submiting I agree to receive job offers from Tesonet</p>
+            <button type="submit" disabled={pristine || invalid}>
+              Submit
+            </button>
+          </form>
+        )}
+      />
+    </Container>
   );
 }
 
-const enhance = connect(
-  state => ({
-    demoValue: demo.selectors.demo(state),
-  }),
-  dispatch => bindActionCreators({ demoAction: demo.actions.demo }, dispatch),
-);
-
-export default enhance(Start);
+export default Start;
